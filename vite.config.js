8d7b578a035f7e438057c9657e0dfb479b13cd12
Alexandru Cristian Robert <alexandru.cristian.robert@gmail.com>
2026-05-10 14:39:45 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 export default defineConfig({
     plugins: [vue()],
+    base: '/GermanTrainer/',
     test: {
         globals: true,
-        base: '/GermanTrainer/',
         environment: 'jsdom',
         setupFiles: ['./tests/setup.ts'],
         include: ['tests/**/*.test.ts']
