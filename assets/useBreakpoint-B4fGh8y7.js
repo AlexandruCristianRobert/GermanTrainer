b28bd56e48f8aac591789be97070d5a68639e8c4
Alexandru Cristian Robert <alexandru.cristian.robert@gmail.com>
2026-08-21 22:03:47 +0300

    Updates

--- assets/useBreakpoint-B4fGh8y7.js
+++
@@ -1,1 +0,0 @@
-import{f as a,l as s}from"./index-C6Li0szq.js";const i="(max-width: 767.99px)";function c(){const e=window.matchMedia(i),n=a(e.matches);function t(o){n.value=o.matches}return e.addEventListener("change",t),s(()=>{e.removeEventListener("change",t)}),{isMobile:n}}export{c as u};
